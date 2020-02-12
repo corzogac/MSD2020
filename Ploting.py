3666a09927a162af4370db9091ac8fbf0f4f3027
Gerald Corzo <gerald.corzo@gmail.com>
2020-02-12 18:19:29 +0100

    Working with Imaginaries

--- Ploting.py
+++ Ploting.py
@@ -1,12 +1,18 @@
 from bokeh.plotting import figure
 from bokeh.io import output_notebook,show
 
+#To be able to plot we define first what type of output
+output_notebook()
+
+f=figure(title="simple line example", x_axis_label='x', y_axis_label='y')
+
 
 def TS(t,y):
-    #To be able to plot we define first what type of output
-    output_notebook()
-
+    
     #now we create the figure 
-    f=figure(title="simple line example", x_axis_label='x', y_axis_label='y')
-    f.circle(t,y,size=3,color='navy',legend="Wave",)
+    f.circle(t,y,size=3,color='navy',legend="Wave")
+    show(f)
+
+def Plot(x,y):
+    f.circle(x,y,size=3,color='navy',legend="Wave")
     show(f)
--- app.py
+++ app.py
@@ -6,3 +6,13 @@ t=np.arange(len(x))
 y=10*np.sin(t/200)
 
 P.TS(t,y)
+
+
+#for imaginary
+a=(2+3j)
+
+m=np.linspace(-1,1,1000)
+X1=m
+X2=1j*m
+
+P.Plot(X1,X2)
