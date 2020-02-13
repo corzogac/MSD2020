744e3fe19051b1eff47c91175e3fc51306b8d04a
Gerald Corzo <gerald.corzo@gmail.com>
2020-02-13 13:16:02 +0100

    Simple updates

--- app.py
+++ app.py
@@ -7,11 +7,7 @@ y=10*np.sin(t/200)
 
 P.TS(t,y)
 
-<<<<<<< HEAD
 from bokeh.plotting import figure
 from bokeh.io import output_notebook,show
 f=figure(title="simple line example", x_axis_label='x', y_axis_label='y')
-=======
-P.Input()
->>>>>>> 8418b699c146f0642c3c5fe856ab7ac61352bb54
 
