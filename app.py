ad06c8e669b166ee74b6ee35f120735bceab597f
Dr F Corzo <fabio.corzo@gmail.com>
2020-02-13 21:28:41 +0000

    intento dos con commit

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ from bokeh.plotting import figure
 from bokeh.io import output_notebook,show
 f=figure(title="simple line example", x_axis_label='x', y_axis_label='y')
 
-# This is a comment
+t=12*12
+
